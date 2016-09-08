cb494f4182dce698f914c20d8ba3cf94248057ec
Fabio Lima <fabiolima39@gmail.com>
2016-09-08 14:01:46 -0300

    support to help,version and error parameters

--- users.sh
+++ users.sh
@@ -9,6 +9,7 @@
 #Description:
 # Show logins and names of the user from system
 # Get information from /etc/passwd
+#	Support to option -h
 #-----------------------------------
 #
 #Example:
@@ -18,11 +19,24 @@
 #
 #History
 #
-#v1.0 2016/09/08, FabioLima
+#v2.0 2016/09/08, FabioLima
 #
 #-----------------------------------
 #
 #License: GPL
 #
 
+MESSAGE_USE="
+Use: $0 [-h]
+	-h Show the windown and close
+"
+
+# options command line
+if test "$1" = "-h"
+then
+	echo "$MESSAGE_USE"
+	exit 0
+fi
+
+#process
 cut -d : -f 1,5 /etc/passwd | tr : \\t
