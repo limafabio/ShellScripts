7c540662f9a3a651f7e684547395a0815ff6ab76
Fabio Lima <fabiolima39@gmail.com>
2016-09-09 12:24:29 -0300

    script to test sound

---
+++ sound.sh
@@ -0,0 +1,38 @@
+#!/usr/bin
+#solved by FabioLima
+#
+#NameScript: sound.sh
+#
+#Author and Maintaining: Fabio Lima
+#
+#-----------------------------------
+#Description:
+#
+#script to test sound 
+#
+#-----------------------------------
+#
+#Example:
+# ./sound.sh
+#
+#-----------------------------------
+#
+#History
+#
+#v1.0 2016/09/09, FabioLima
+#
+#-----------------------------------
+#
+#License: GPL
+#
+	
+Debug(){
+	[ $1 -le $DEBUG ] && echo "--- DEBUG $*" 
+}
+
+echo -ne "\033[11;900]"
+while :
+do
+	echo -ne "\033[10;500]\a" ; sleep 1
+	echo -ne "\033[10;400]\a" ; sleep 1
+done
