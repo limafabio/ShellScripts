56737174390b6f4f974b18716f16ef9d12e17a8c
Fabio Lima <fabiolima39@gmail.com>
2016-09-10 20:26:47 -0300

    script to get info from site

---
+++ getinfo.sh
@@ -0,0 +1,43 @@
+#!/usr/bin
+#solved by FabioLima
+#
+#NameScript:	getinfo.sh
+#
+#Author and Maintaining: Fabio Lima
+#
+#-----------------------------------
+#Description:
+#Script to get information from the web site brlinux.org
+#it's necessary install lynx
+#-----------------------------------
+#
+#Example:
+# ./getinfo.sh
+#
+#-----------------------------------
+#
+#History
+#
+#v1.0 2016/09/10, FabioLima
+#
+#-----------------------------------
+#
+#License: GPL
+#
+	
+Debug(){
+	[ $1 -le $DEBUG ] && echo "--- DEBUG $*" 
+}
+
+
+URL="http://br-linux.org"
+
+hash lynx 2>/dev/null || { 
+	echo >&2 "It's require lynx but it's not installed. Aborting."; exit 1;
+}
+
+lynx -dump -nolist "$URL" |
+	grep '^[A-Z]' |
+	head -n 10
+
+
